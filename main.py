9191e85355c91ef4612d34e427f28c42021da239
lesbpawoaku <may2006@proton.me>
2026-06-15 20:24:58 -0500

    random goals and starts added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from gameplay import play
+from pickwords import pick
 
 
 def main():
-    curr_word = "mark"
-    goal_word = "turd"
+    curr_word, goal_word = pick()
     the_print = f"GOAL: {goal_word} \n({curr_word[0]}) ({curr_word[1]}) ({curr_word[2]}) ({curr_word[3]})"
     guesses = 0
     play(curr_word, goal_word, the_print, guesses)
---
+++ pickwords.py
@@ -0,0 +1,23 @@
+import random
+
+from check import check
+from word_list import word_list
+
+
+def pick():
+    goal_word = random.choice(word_list)
+    new_word = random.choice(word_list)
+
+    def new_word_check(new_word, goal_word, words_away):
+        if words_away > 5:
+            checker = check(new_word, goal_word, goal_word)
+            match checker:
+                case "valid":
+                    new_word_check(random.choice(word_list), new_word, words_away + 1)
+                case _:
+                    new_word_check(random.choice(word_list), goal_word, words_away)
+        else:
+            return new_word
+
+    first_word = new_word_check(new_word, goal_word, 0)
+    return first_word, goal_word
